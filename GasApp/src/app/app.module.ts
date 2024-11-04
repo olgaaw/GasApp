1f2ae452c657035714a8994ddf0e8b1603d4b02b
David Sevillano <dvd.sevillano@gmail.com>
2024-11-04 11:36:58 +0100

    Creacion carpetas

--- GasApp/src/app/app.module.ts
+++ GasApp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GasListComponent } from './components/gas-list/gas-list.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GasListComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ GasApp/src/app/components/gas-list/gas-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gas-list',
+  templateUrl: './gas-list.component.html',
+  styleUrl: './gas-list.component.css'
+})
+export class GasListComponent {
+
+}
