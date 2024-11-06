17b599d3a34513bdd4f1dd089286ff22af8fa432
Olga <olga.valor.wu@gmail.com>
2024-11-06 01:12:17 +0100

    filtros

--- GasApp/src/app/components/gas-list/gas-list.component.ts
+++ GasApp/src/app/components/gas-list/gas-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, TemplateRef } from '@angular/core';
 import { GasService } from '../../services/gas.service';
 import { Gasolinera } from '../../models/gas-item.dto';
+import { CarburantesList } from '../../models/carburantes-list.interface';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { ComunidadesAutonomas } from '../../models/comunidades-list.interface';
 
 @Component({
   selector: 'app-gas-list',
@@ -9,8 +12,8 @@ import { Gasolinera } from '../../models/gas-item.dto';
 })
 export class GasListComponent implements OnInit{
   listaGasolineras: Gasolinera[] = [];
-
-
+  listaCarburantes: CarburantesList[] = [];
+  listaComunidades: ComunidadesAutonomas[] = [];
 
   constructor(private gasService: GasService) {}
 
@@ -27,7 +30,17 @@ export class GasListComponent implements OnInit{
         }
       });
 
-  }
+      this.gasService.getCarburantesList().subscribe((resp) => {
+        this.listaCarburantes = resp;
+      });
+
+      this.gasService.getCcAaList().subscribe((resp) => {
+        this.listaComunidades = resp;
+      });
+      
+
+    }
+
     private cleanProperties(arrayGasolineras: any) {
       let newArray: Gasolinera[] = [];
       arrayGasolineras.forEach((gasolineraChusquera: any) => {
@@ -48,6 +61,12 @@ export class GasListComponent implements OnInit{
         newArray.push(gasolinera);
       });
       return newArray;
+    }
+
+    private offcanvasService = inject(NgbOffcanvas);
+
+    openEnd(content: TemplateRef<any>) {
+      this.offcanvasService.open(content, { position: 'end' });
     }
   }
 
